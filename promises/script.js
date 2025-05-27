f10dc8ac01c247a71eb07c5a84814566335ed70e
emmeiwhite <emmeiwhite@gmail.com>
2025-05-27 12:42:16 +0530

    2. promise resolve example

--- promises/script.js
+++ promises/script.js
@@ -6,3 +6,16 @@ fetch('https://dummyjson.com/products')
   .then(console.log)
 
 console.log(a + b)
+
+/** Q3. Write a function that returns a promise which resolves after n milliseconds? */
+
+const delay = ms => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('data served')
+    }, ms)
+  })
+}
+
+// Usage
+delay(1000).then(res => console.log(res))
