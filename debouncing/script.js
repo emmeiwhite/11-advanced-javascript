6066fd73acbf2086145f9c0cb22955b8bc977cdb
emmeiwhite <emmeiwhite@gmail.com>
2025-06-21 19:35:36 +0530

    9. debouncedSearch - a treat to watch

--- debouncing/script.js
+++ debouncing/script.js
@@ -15,3 +15,26 @@ search('Hard J')
 search('Hard JS')
 
 // On each letter the user types, the API is called
+console.log('---------***** A Debounced Version *****--------')
+// We will now create a debounce function which takes search function and delay as an argument
+
+function debounce(fn, delay) {
+  let timerId
+  // closure in action again
+  return function (...args) {
+    clearTimeout(timerId) // cancel the last call
+    timerId = setTimeout(() => {
+      fn(...args)
+    }, delay)
+  }
+}
+
+const searchWithDebounce = debounce(search, 1000) // Returns a function
+
+searchWithDebounce('H')
+searchWithDebounce('Ha')
+searchWithDebounce('Har')
+searchWithDebounce('Hard')
+searchWithDebounce('Hard ')
+searchWithDebounce('Hard J')
+searchWithDebounce('Hard JS')
