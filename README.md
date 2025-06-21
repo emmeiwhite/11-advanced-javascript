1751153b5dbbcbc9fc2aab517223e625a3f956d2
emmeiwhite <emmeiwhite@gmail.com>
2025-06-21 15:22:02 +0530

    5. closure example --- increment

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Advanced JavaScript Concepts
 
 - Promises
-- Closures
+- Closures: When an inner function has access to the outer function scope, then the inner function along with the outer lexical scope forms a closure. The inner function can access the outer function variables even after the outer function has returned and finished its execution.
--- closures/script.js
+++ closures/script.js
@@ -1,5 +1,6 @@
+/* --- 
+let count = 0 
 function increment() {
-  let count = 0
   count += 1
   return count
 }
@@ -7,3 +8,20 @@ function increment() {
 console.log(increment())
 console.log(increment())
 console.log(increment())
+
+Here we are polluting the global scope, now advised!
+--- */
+
+function increment() {
+  let count = 0
+  return function () {
+    count += 1
+    return count
+  }
+}
+
+const myInc = increment() // At this point, myInc function still has access to the lexical environment, where count is initialized. count=0;
+
+console.log(myInc()) // count: 0--->0+1 = 1
+console.log(myInc()) // count: 1--->1+1 = 2
+console.log(myInc()) // count: 2--->2+1 = 3
