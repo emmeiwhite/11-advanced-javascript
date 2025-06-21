dbcbabb098b6e6967bdecb08f436cf8616f140cb
emmeiwhite <emmeiwhite@gmail.com>
2025-06-21 18:57:59 +0530

    7. memoization in progress, caching the results to improve performance

--- memoize/script.js
+++ memoize/script.js
@@ -7,11 +7,36 @@ function simpleSquare() {
   }
 }
 
-const memoSquare = simpleSquare()
+const square = simpleSquare()
 // Non Memoized Simple Square, return n*n; and this calculation happens on every call
-console.log(memoSquare(5))
-console.log(memoSquare(5))
-console.log(memoSquare(5))
-console.log(memoSquare(5))
-console.log(memoSquare(5))
-console.log(memoSquare(5))
+console.log(square(5))
+console.log(square(5))
+console.log(square(5))
+console.log(square(5))
+console.log(square(5))
+console.log(square(5))
+
+console.log('-------********** Square Memoized *********** ---------')
+
+function memoizedSquare() {
+  const cache = {} // cache object
+  return function (n) {
+    if (n in cache) {
+      console.log('Memoized/Cached value returned!')
+      return cache[n]
+    }
+
+    console.log('Calculated and setup value initial process')
+    cache[n] = n * n
+    return cache[n]
+  }
+}
+
+const memoSquare = memoizedSquare()
+
+console.log(memoSquare(7))
+console.log(memoSquare(7))
+console.log(memoSquare(7))
+console.log(memoSquare(7))
+console.log(memoSquare(7))
+console.log(memoSquare(7))
