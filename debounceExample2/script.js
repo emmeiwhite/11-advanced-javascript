c50d3a58cffd65c720e8ee55cc5cc3d49b580136
emmeiwhite <emmeiwhite@gmail.com>
2025-06-29 20:08:45 +0530

    13. Deboucned Function --- Done!

--- debounceExample2/script.js
+++ debounceExample2/script.js
@@ -4,7 +4,22 @@ const prodContainer = document.querySelector('.products-container')
 // Step-1: function which makes API call
 
 function searchProducts(e) {
-  console.log(e.target.value)
+  console.log('Calling API', e.target.value)
 }
 
-inputElem.addEventListener('input', searchProducts)
+function debounce(func, delay) {
+  let timerID
+  return function (...args) {
+    // rest operator
+    clearTimeout(timerID)
+    timerID = setTimeout(() => {
+      func(...args) // spread operator
+    }, delay)
+  }
+}
+
+const debouncedSearch = debounce(searchProducts, 1000)
+
+// Step-2: Debounced Function - To call the searchProducts only once user stops typing for a brief period, That's when we'll make an API call
+
+inputElem.addEventListener('input', debouncedSearch)
