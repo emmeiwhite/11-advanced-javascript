4c2bd1e67bb9284118ba9e3e1d3062cf72448667
emmeiwhite <emmeiwhite@gmail.com>
2025-06-30 22:34:39 +0530

    24. Event Delegation  --- Implemented

--- event-propagation/script.js
+++ event-propagation/script.js
@@ -24,3 +24,30 @@ parent.addEventListener(
   },
   true
 )
+
+// Event-Delegation: One Parent Handler to rule them all
+
+const fruitList = document.querySelector('.fruit-list')
+const addNewBtn = document.querySelector('.add-new')
+
+fruitList.addEventListener('click', e => {
+  if (e.target.tagName === 'LI') {
+    console.log('list-item clicked: ', e.target.innerText)
+  }
+})
+
+addNewBtn.addEventListener('click', e => {
+  const btn = e.target
+  btn.disabled = true
+  btn.textContent = 'Adding...'
+
+  setTimeout(() => {
+    const listItem = document.createElement('li')
+    listItem.textContent = 'New Fruit'
+    fruitList.appendChild(listItem)
+
+    // Reset button after 1 second
+    e.target.disabled = false
+    btn.textContent = 'Add new Item'
+  }, 1000)
+})
