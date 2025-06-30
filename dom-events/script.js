ee68182a43787c9faec0e59ec381d9268db67495
emmeiwhite <emmeiwhite@gmail.com>
2025-06-30 16:18:18 +0530

    20. clearInterval() to release resource on node destroy

--- dom-events/script.js
+++ dom-events/script.js
@@ -1,5 +1,5 @@
 // 1. Memory Leaks (in this example)
-
+/** ---
 const div = document.createElement('div')
 div.textContent = 'I am created on the fly'
 
@@ -11,3 +11,22 @@ setInterval(() => {
 
 // Later...
 document.body.removeChild(div) // ❗ But interval still runs!
+--- */
+
+const div = document.createElement('div')
+div.textContent = 'I am created on the fly'
+
+document.body.appendChild(div)
+
+const intervalId = setInterval(() => {
+  console.log('🌟 Running 🏃🏃🏃!')
+}, 1000)
+
+console.log(intervalId)
+
+// Clean up
+setTimeout(() => {
+  clearInterval(intervalId)
+  document.body.removeChild(div)
+  console.log('🧼 Cleaned!')
+}, 5000)
