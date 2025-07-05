4ed2492f15614a2564ca62bb2071494d1717cf48
emmeiwhite <emmeiwhite@gmail.com>
2025-07-05 23:23:01 +0530

    26. Constructor Function and protype

--- oop/script.js
+++ oop/script.js
@@ -1,3 +1,4 @@
+/** 1. Every object has a __proto__ property pointing to the prototype of the object */
 const person = {
   name: 'Imran',
   greet() {
@@ -6,3 +7,18 @@ const person = {
 }
 
 console.log(person.__proto__) // points to Object.prototype
+
+/** 2. Every function has a .prototype property property, and when the function is used as the contructor function to create objects, this .prototype becomes the object's internal __proto__ */
+
+function Person(name) {
+  this.name = name
+}
+
+Person.prototype.greet = function () {
+  console.log(`Hello ${this.name}`)
+}
+
+const person1 = new Person('Emmei')
+person1.greet()
+
+console.log(Person.prototype === person1.__proto__)
