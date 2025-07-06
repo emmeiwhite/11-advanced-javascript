9f3773448b46dd7006516c633c2026d98bfbd95b
emmeiwhite <emmeiwhite@gmail.com>
2025-07-06 15:27:28 +0530

    33. const phonePrice; SyntaxError

--- errors/script.js
+++ errors/script.js
@@ -11,10 +11,15 @@ try {
   console.error(error.message)
 }
 
-// 2. SyntaxError: When you write a piece of code that JS cannot understand
+// 2. SyntaxError: When you write a piece of code that JS cannot understand. Note that we cannot handle these SyntaxErrors due to these errors occuring in the compile phase (Creation Phase)
+/*
 try {
     let word = "Keep Moving... // SyntaxError: Invalid or unexpected token
 } catch (error) {
     console.error(error.message)
 }
 
+*/
+
+
+const phonePrice; // Uncaught SyntaxError: Missing initializer in const declaration
