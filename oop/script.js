bf31531881b8fe97a6c787045b9da6ea65a70e34
emmeiwhite <emmeiwhite@gmail.com>
2025-07-06 14:36:37 +0530

    29. resusing function as methods

--- oop/script.js
+++ oop/script.js
@@ -1,5 +1,5 @@
-// The function uses this keyword. The value of this depends on how greet is invoked
-const greet = msg => {
+// 1. The function uses this keyword. The value of this depends on how greet is invoked
+function greet(msg) {
   console.log(`${msg}, ${this.userName}`)
 }
 
@@ -7,4 +7,27 @@ const person = {
   userName: 'Tomi'
 }
 
-greet.call(person, 'hello') // hello, undefined, because arrow fxn does not have their this keyword
+greet.call(person, 'Hello')
+// hello, undefined, because arrow fxn does not have their this keyword
+
+greet.apply(person, ['Hi'])
+
+// bind(): returns a new function with fixed "this"
+
+const boundGreet = greet.bind(person, 'Hey!')
+boundGreet()
+
+// 2. Scenario-2 is that we can reuse a function with call, apply
+
+function getDetails() {
+  return this.name + ' ' + this.age
+}
+
+const emmei = { name: 'Emmei', age: '32', getDetails }
+const rawa = { name: 'Rawa', age: '27', getDetails }
+
+const emmeiDetails = emmei.getDetails()
+const rawaDetails = rawa.getDetails()
+
+console.log(emmeiDetails)
+console.log(rawaDetails)
