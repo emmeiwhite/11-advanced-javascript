d78923795245ccb60957d76c7c1f6c0c72a65104
emmeiwhite <emmeiwhite@gmail.com>
2025-07-21 15:19:14 +0530

    37. Function expressions do not have their own this

--- this-keyword/script.js
+++ this-keyword/script.js
@@ -6,3 +6,10 @@ function normalFxn() {
 }
 
 normalFxn()
+
+// Function expressions do have their own this, they use this from the outer scope or outer execution context
+const fxn2 = () => {
+  console.log(this)
+}
+
+fxn2()
