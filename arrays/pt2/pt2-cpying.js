1e2731d7ad9dfb83b9ec8eef513414ddd0b7e0a3
emmeiwhite <emmeiwhite@gmail.com>
2025-07-31 11:55:51 +0530

    49. Objects are stored in separate place only their references are stored in indexes. DeepClone performed!

--- arrays/pt2/pt2-cpying.js
+++ arrays/pt2/pt2-cpying.js
@@ -5,3 +5,16 @@ copyOfNums
 copyOfNums[1] = 56
 copyOfNums
 arrOfNums
+
+// Array of Objects
+const arrOfObjects = [{ name: 'Jack' }, { name: 'doe' }]
+const copyObjects = [...arrOfObjects]
+copyObjects
+
+// JavaScript is reference based in order to maintain performant nature of JavaScript, and in order to perform clone we can use structuredClone() from ES7(I believe) or _.deepClone() from loadash
+
+const deepCloneObjects = structuredClone(arrOfObjects)
+deepCloneObjects[0].name = 'Imran'
+
+deepCloneObjects
+arrOfObjects
